8497471a7dc5c985044582508e4ecba82203ea8a
root <root@ip-172-31-93-220.ec2.internal>
2024-04-24 04:08:43 +0000

    initial push

---
+++ src/components/Hero.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+/*import logo from "../assets/logo.png";*/
+
+export default function HeroImage() {
+  return (
+    <header style={{ paddingLeft: 0 }}>
+      <div
+        className='p-5 text-center bg-image'
+        style={{ backgroundImage: "url('https://okta.poc.gmoonmedia.com/homepage-banner-2.jpg')", height: 400 }}
+      >
+        <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-white'>
+              <h1 className='mb-3'>Welcome Aboard</h1>
+              <h4 className='mb-3'>Subheading</h4>
+              <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
+                Call to action
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+/*const Hero = () => (
+  <div className="text-center hero my-5">
+    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+    <h1 className="mb-4">React.js Sample Project</h1>
+
+    <p className="lead">
+      This is a sample application that demonstrates an authentication flow for
+      an SPA, using <a href="https://reactjs.org">React.js</a>
+    </p>
+  </div>
+);
+
+export default Hero;*/
